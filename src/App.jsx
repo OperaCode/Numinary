8ea855e90bc4365928d1bdad9efe5fab34a55ebb
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-07-15 16:02:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+import Landing from './components/Landing'
+import Home from './components/Home'
+import { Route,Routes } from 'react-router-dom'
+
+function App() {
+  
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/home' element={<Home/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
